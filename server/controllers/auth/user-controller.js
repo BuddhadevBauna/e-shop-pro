7e27468e9abccbdb083a0c0ff4912d16c35da866
BuddhadevBauna <baunabuddhadev2001@gmail.com>
2024-06-27 11:54:36 +0530

    added usercontroller

---
+++ server/controllers/auth/user-controller.js
@@ -0,0 +1,13 @@
+const user = async (req, res) => {
+    try {
+        const userData = req.userData;
+        // console.log(userData);
+        return res
+            .status(200)
+            .json({ userData })
+    } catch (error) {
+        console.log(`error from user route ${error}`);
+    }
+}
+
+export default user;
--- server/routes/auth-router.js
+++ server/routes/auth-router.js
@@ -3,10 +3,13 @@ import register from "../controllers/auth/register-controller.js";
 import login from "../controllers/auth/login-controller.js";
 import validate from "../middlewares/validate-middleware.js";
 import { signinSchema, signupSchema } from "../validators/auth-validator.js";
+import authMiddleware from "../middlewares/auth-middleware.js";
+import user from "../controllers/auth/user-controller.js";
 
 const router = express.Router();
 
 router.route('/register').post(validate(signupSchema), register);
 router.route('/login').post(validate(signinSchema), login);
+router.route('/user').get(authMiddleware, user);
 
 export default router;

e42ddeacfb4730b711d1fd48618f9caaa1c40e51
Buddhadev Bauna <baunabuddhadev2001@gmail.com>
2024-07-02 20:42:43 +0530

    add get Particular Category Or SubCategory conroller

--- server/controllers/categories/get-category-controller.js
+++ server/controllers/categories/get-category-controller.js
@@ -3,10 +3,37 @@ import Category from "../../models/category-model.js";
 //get all Category
 export const getAllCategory = async (req, res) => {
     try {
-        const products = await Category.find({});
-        return res.status(200).json(products);
+        const categories = await Category.find({});
+        return res.status(200).json(categories);
     } catch (error) {
         console.error(error);
-        return res.status(500).json({message: "All product get unsucessful"});
+        return res.status(500).json({ message: "Get all category unsucessful" });
+    }
+}
+
+//get particular category or subcategory
+export const getParticularCategoryOrSubCategory = async (req, res) => {
+    try {
+        const { categoryId, subCategoryId } = req.query;
+
+        if (!subCategoryId) {
+            let category = await Category.findOne({ _id: categoryId });
+            if (!category) {
+                return res.status(404).json({ message: "Category not found" });
+            }
+            return res.status(200).json(category);
+        } else {
+            const subCategory = await Category.findOne(
+                { _id: categoryId, "subCategory._id": subCategoryId },
+                { "subCategory.$": 1 } // Return only the matched subCategory
+            );
+            if (!subCategory) {
+                return res.status(404).json({ message: "Sub Category not found" });
+            }
+            return res.status(200).json(subCategory);
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Get particular category or subcategory unsucessful" });
     }
 }
--- server/routes/category-router.js
+++ server/routes/category-router.js
@@ -1,6 +1,6 @@
 import express from "express";
 import addCategory from "../controllers/categories/add-category-controller.js";
-import { getAllCategory } from "../controllers/categories/get-category-controller.js";
+import * as getCategory from "../controllers/categories/get-category-controller.js";
 import categoryUpdate from "../controllers/categories/update-category-controller.js";
 import authMiddleware from "../middlewares/auth-middleware.js";
 import adminMiddleware from "../middlewares/admin-middleware.js";
@@ -16,8 +16,10 @@ router.route('/update/q').patch(authMiddleware, adminMiddleware, categoryUpdate)
 //delete category
 router.route('/delete/q').delete(authMiddleware, adminMiddleware, deleteCategory);
 
-//get Category
-router.route('/').get(getAllCategory);
+//get all category
+router.route('/').get(getCategory.getAllCategory);
+//get particular category or subcategory
+router.route('/q').get(getCategory.getParticularCategoryOrSubCategory);
 
 
 export default router;

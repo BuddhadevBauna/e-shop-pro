febcbe342539f97c1410c30e5b7044237f67df9c
Buddhadev Bauna <baunabuddhadev2001@gmail.com>
2024-07-11 23:18:45 +0530

    add getProductsOfCartOfParticularUser controller

---
+++ server/controllers/cart/get-products-of-cart-of-particular-user-controler.js
@@ -0,0 +1,17 @@
+import Cart from "../../models/cart-model.js";
+
+const productsOfCartOfParticularUser = async (req, res) => {
+    try {
+        const {useremail} = req.body;
+
+        const cartItems = await Cart.findOne({useremail : useremail});
+        if(cartItems) {
+            return res.status(200).json(cartItems);
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message : "get cart items unsucessful"});
+    }
+}
+
+export default productsOfCartOfParticularUser;
--- server/routes/cart-route.js
+++ server/routes/cart-route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import addProductInCart from "../controllers/cart/add-product-in-cart-controller.js";
 import authMiddleware from "../middlewares/auth-middleware.js";
+import productsOfCartOfParticularUser from "../controllers/cart/get-products-of-cart-of-particular-user-controler.js";
 
 const router = express.Router();
 
 //add product in cart
 router.route('/add').post(authMiddleware, addProductInCart);
 //get cart item of particular user
-router.route('').get();
+router.route('').get(authMiddleware, productsOfCartOfParticularUser);
 
 export default router;
